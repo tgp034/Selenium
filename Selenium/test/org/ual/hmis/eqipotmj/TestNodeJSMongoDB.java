bdc382606df35636c654961803d3d8cc19a0deeb
Tito Julio González Padial <72378065+tgp034@users.noreply.github.com>
2022-06-03 11:44:46 +0200

    Update TestNodeJSMongoDB.java

--- Selenium/test/org/ual/hmis/eqipotmj/TestNodeJSMongoDB.java
+++ Selenium/test/org/ual/hmis/eqipotmj/TestNodeJSMongoDB.java
@@ -36,7 +36,7 @@ public class TestNodeJSMongoDB {
 			// Descargar geckodriver de https://github.com/mozilla/geckodriver/releases
 			// Descomprimir el archivo geckodriver.exe en la carpeta drivers
 
-			System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
 			FirefoxOptions firefoxOptions = new FirefoxOptions();
 			if (headless)
 				firefoxOptions.setHeadless(headless);
